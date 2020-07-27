000d0f2db4d59c2cf8751b86207916c649e5f918
PabloEnriquez <12pable34@gmail.com>
2020-07-27 00:02:25 -0500

    modulo config completo

--- src/app/modules/config/config-user/config-user.component.ts
+++ src/app/modules/config/config-user/config-user.component.ts
@@ -1,19 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subscription } from 'rxjs';
 import { SocketService } from '../../../services/socket.service';
+import { NewUser } from 'src/app/models/models';
 
 @Component({
   selector: 'app-config-user',
   templateUrl: './config-user.component.html',
   styleUrls: ['./config-user.component.sass']
 })
-export class ConfigUserComponent implements OnInit {
+export class ConfigUserComponent implements OnInit, OnDestroy {
+
+  userData: NewUser = { userId: null, userName: '' };
+  activeUserSub: Subscription;
 
   constructor(
-    private socketService: SocketService
+    private socketService: SocketService,
+    private snackBar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
+    this.socketService.getActiveUserName();
+    this.activeUserSub = this.socketService.userData$.subscribe(userData => {
+      if ( userData ) {
+        this.userData = userData;
+      }
+    });
+  }
 
+  getRandomId(): number {
+    return Math.floor((1 + Math.random()) * 0x10000);
+  }
+
+  onSave(): void {
+    const newData: NewUser = {
+      userId: this.userData && this.userData.userId ? this.userData.userId : this.getRandomId(),
+      userName: this.userData.userName
+    };
+    this.socketService.setActiveUserName(newData);
+    this.snackBar.open('Guardado exitoso', 'Cerrar', {
+      duration: 500,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.activeUserSub.unsubscribe();
   }
 
 }
