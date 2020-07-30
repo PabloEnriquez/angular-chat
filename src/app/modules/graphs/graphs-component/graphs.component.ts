09e978f97c21f265940dfc0a54ff4dddf99d5759
PabloEnriquez <12pable34@gmail.com>
2020-07-30 01:34:28 -0500

    vista gráficas. componentes reusables

--- src/app/modules/graphs/graphs-component/graphs.component.ts
+++ src/app/modules/graphs/graphs-component/graphs.component.ts
@@ -1,36 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { GraphData } from 'src/app/models/models';
+import { GraphsService } from './../../../services/graphs.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-graphs',
   templateUrl: './graphs.component.html',
   styleUrls: ['./graphs.component.sass']
 })
-export class GraphsComponent implements OnInit {
+export class GraphsComponent implements OnInit, OnDestroy {
 
-  graphOne: GraphData;
+  graphs: GraphData[] = [];
+  graphsSub: Subscription;
 
-  constructor() { }
+  constructor(private graphsService: GraphsService) { }
 
   ngOnInit(): void {
-    this.graphOne = {
-      lineChartData: [65, 59, 80, 81, 56, 55, 40, 34, 81, 56, 55, 40, 34],
-      lineChartColor: {
-        backgroundColor: 'rgba(77,83,96,0.2)',
-        borderColor: 'rgba(77,83,96,1)'
-      },
-      doughnutChartData: [ 80000, 120000 ],
-      doughnutColor: {
-        backgroundColor: ['#5BC790', '#ECB741']
-      },
-      txtCategory: 'REVENUE',
-      quantity: 200000,
-      isRevenue: true,
-      leftLabel: 'Tablet',
-      leftData: 120000,
-      rightLabel: 'Smartphone',
-      righData: 80000
-    };
+    this.graphsService.getGraphsData();
+    this.graphsSub = this.graphsService.graphList$.subscribe(list => {
+      if ( list ) {
+        this.graphs = list;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.graphsSub.unsubscribe();
   }
 
 }
